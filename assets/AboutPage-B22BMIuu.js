177d656d4832a075390b231792135d6300968027
ZICO <ziyodahmedov23@gmail.com>
2024-07-05 21:51:39 +0500

    test 4

---
+++ assets/AboutPage-B22BMIuu.js
@@ -0,0 +1,1 @@
+import{c as a,o as n,a as r}from"./index-DkXfuvoH.js";const _=(t,e)=>{const o=t.__vccOpts||t;for(const[s,c]of e)o[s]=c;return o},i={},d={class:"about"},l=r("h1",null,"This is an about page",-1),u=[l];function f(t,e){return n(),a("div",d,u)}const p=_(i,[["render",f]]);export{p as default};
